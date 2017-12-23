09c2e41dde6ad00fe39f8b5c9c0f56474c29d2d7
MdAbedin <mabedin1@stuy.edu>
2017-12-23 17:24:34 -0500

    colors and illegal move detection

--- iago.c
+++ iago.c
@@ -16,6 +16,7 @@
 #define gotoxy(x,y) printf("\033[%d;%dH", (y+1), (x+1))
 //sets cursor position to the middle of the given (x,y) tile
 #define gotoBoardXY(x,y) gotoxy(4*x+2, 2*y+1)
+#define clearLine() printf("\033[0m\033[K\033[42m");
 
 #define UP 'A'
 #define DOWN 'B'
@@ -34,6 +35,14 @@ struct termios initial_settings,
 int current_x = 0;
 int current_y = 0;
 
+//changes a piece of board[][]
+void place_piece(int x, int y, char piece){
+  board[y][x] = piece;
+  gotoBoardXY(x,y);
+  if(piece == 'b') printf("\033[30mb");
+  if(piece == 'w') printf("\033[97mw");
+}
+
 //sets board[][] to all spaces, then adds initial pieces
 void initialize(){
   int y;
@@ -44,18 +53,22 @@ void initialize(){
     }
   }
 
-  board[3][3] = 'w';
-  board[3][4] = 'b';
-  board[4][3] = 'b';
-  board[4][4] = 'w';
+  place_piece(3,3,'w');
+  place_piece(3,4,'b');
+  place_piece(4,3,'b');
+  place_piece(4,4,'w');
 }
 
 //prints empty board
 void print_board(){
+  printf("\033[30m");
+  printf("\033[42m");
   int file = open("board.txt", O_RDONLY);
   char buffer[1024];
   read(file, buffer, sizeof(buffer));
   printf("%s", buffer);
+
+  //printf("\033[0m");
 }
 
 //prints current configuration of board[][]
@@ -65,14 +78,12 @@ void update_board(){
   for(y = 0; y < 8; y++){
     for(x = 0; x < 8; x++){
       gotoBoardXY(x, y);
+      if(board[y][x] == 'b') printf("\033[1;30m");
+      //if(board[y][x] == ' ') printf("\033[1;42m");
+	
       printf("%c", board[y][x]);
     }
   }
-}
-
-//changes a piece of board[][]
-void place_piece(int x, int y, char piece){
-  board[y][x] = piece;
 }
 
 //checks if (x,y) is in the board
@@ -97,6 +108,24 @@ int conquer_count(int x, int y, int xDir, int yDir, char piece){
 
   if(!inBounds(x,y) || board[y][x] != piece) count = 0;
   return count;
+}
+
+//checks if it's legal to place piece at (x,y)
+int isLegal(int x, int y, char piece){
+  if(board[y][x] != ' ') return 0;
+
+  int xDir;
+  int yDir;
+  
+  for(yDir = -1; yDir < 2; yDir++){
+    for(xDir = -1; xDir < 2; xDir++){
+      if(xDir == 0 && yDir == 0) continue;
+      
+      if(conquer_count(x,y,xDir,yDir, piece)) return 1;
+    }
+  }
+
+  return 0;
 }
 
 //flips all pieces that should be flipped if piece is placed at (x,y)
@@ -169,46 +198,63 @@ void move(){
       key = n;
       if(key == UP){
 	move_up();
+	gotoBoardXY(0,9);
+	clearLine();
       }
       else if(key == DOWN){
 	move_down();
+	gotoBoardXY(0,9);
+	clearLine();
       }
       else if(key == RIGHT){
 	move_right();
+	gotoBoardXY(0,9);
+	clearLine();
       }
       else if(key == LEFT){
 	move_left();
+	gotoBoardXY(0,9);
+	clearLine();
       }
       else if(key == B){
-	place_piece(current_x, current_y, 'b');
-	conquer_pieces(current_x, current_y, 'b');
-	update_board();
+	if(isLegal(current_x, current_y, 'b')){
+	  place_piece(current_x, current_y, 'b');
+	  conquer_pieces(current_x, current_y, 'b');
+	  gotoBoardXY(0,9);
+	  printf("\033[0mplaced a black piece at (%d, %d)\033[42m", current_x, current_y);
+	}
+	else{
+	  gotoBoardXY(0,9);
+	  printf("\033[0mcan't place a black piece at (%d, %d)\033[42m", current_x, current_y);
+	}
       }
       else if(key == W){
-	place_piece(current_x, current_y, 'w');
-	conquer_pieces(current_x, current_y, 'w');
-	update_board();
+	if(isLegal(current_x, current_y, 'w')){
+	  place_piece(current_x, current_y, 'w');
+	  conquer_pieces(current_x, current_y, 'w');
+	  gotoBoardXY(0, 9);
+	  printf("\033[0mplaced a white piece at (%d, %d)\033[42m", current_x, current_y);
+	}
+	else{
+	  gotoBoardXY(0,9);
+	  printf("\033[0mcan't place a white piece at (%d, %d)\033[42m", current_x, current_y);
+	}
       }
       else if(key == QUIT){
-	printf("you rage quit\n");
+	printf("\033[0myou rage quit\n");
 	break;
       }
     }
 
-    gotoBoardXY(0,9);
-    printf("%d", conquer_count(current_x, current_y, 1, 0, 'b'));
-	
     gotoBoardXY(current_x, current_y);
   }
   tcsetattr(0, TCSANOW, &initial_settings);
 }
 
 int main(){
-  initialize();
-  clear();
   gotoxy(0,0);
   print_board();
-  update_board();
+  initialize();
 
   move();
 }
