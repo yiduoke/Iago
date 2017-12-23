a832b1a8e03d7061693c075b6c88e1767d9f55ae
MdAbedin <mabedin1@stuy.edu>
2017-12-22 23:38:41 -0500

    board updates

--- iago.c
+++ iago.c
@@ -9,36 +9,63 @@
 #include <sys/sem.h>
 #include <sys/shm.h>
 #include <errno.h>
-#include <termios.h>
 
 #define clear() printf("\033[2J");
 #define hide_cursor() printf("\033[?25l");
-#define gotoxy(x,y) printf("\033[%d;%dH", (x), (y))
-#define UP "w"
-#define DOWN "S"
-#define RIGHT "^[[C"
-#define LEFT "^[[D"
+#define gotoxy(x,y) printf("\033[%d;%dH", (y+1), (x+1))
+//sets cursor position to the middle of the given (x,y) tile
+#define gotoBoardXY(x,y) gotoxy(4*x+2, 2*y+1)
 
-  void print_board(){
+char board[8][8];
+
+//sets board[][] to all spaces, then adds initial pieces
+void initialize(){
+  int y;
+  int x;
+  for(y = 0; y < 8; y++){
+    for(x = 0; x < 8; x++){
+      board[y][x] = ' ';
+    }
+  }
+
+  board[3][3] = 'w';
+  board[3][4] = 'b';
+  board[4][3] = 'b';
+  board[4][4] = 'w';
+}
+
+//prints empty board
+void print_board(){
     int file = open("board.txt", O_RDONLY);
     char buffer[1024];
     read(file, buffer, sizeof(buffer));
     printf("%s", buffer);
 }
 
-void move(){
-    char *input = (char *)calloc(1, 1024);//when in doubt, calloc is always the answer
-    // fgets(input, 2, stdin);
-    scanf("%1s", input);
-    printf("input: [%s]\n", input);
-    // if(!strncmp(UP, input, 1)) printf("up pressed\n");
+//prints current configuration of board[][]
+void update_board(){
+  int y;
+  int x;
+  for(y = 0; y < 8; y++){
+    for(x = 0; x < 8; x++){
+      gotoBoardXY(x, y);
+      printf("%c", board[y][x]);
+    }
+  }
 }
+
+//changes a piece of board[][]
+void place_piece(int x, int y, char piece){
+  board[y][x] = piece;
+}
+
 int main(){
-    hide_cursor();
-    clear();
-    // while(1){
-        gotoxy(0,0);
-        print_board();
-        move();
-    // }
+  initialize();
+  hide_cursor();
+  clear();
+  print_board();
+  
+  while(1){
+    update_board();
+  }
 }
